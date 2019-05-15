e30aa7bf6259ab0119c67fcdd2ebe80aa6a5cac6
Thomas S Hatch <thatch45@gmail.com>
2019-05-15 16:51:31 -0600

    Add demo dir for some integrations

---
+++ demo/salt/beacons/allproc.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+'''
+Send events covering process status
+'''
+
+# Import Python Libs
+from __future__ import absolute_import, unicode_literals
+import logging
+
+# Import third party libs
+# pylint: disable=import-error
+try:
+    import salt.utils.psutil_compat as psutil
+    HAS_PSUTIL = True
+except ImportError:
+    HAS_PSUTIL = False
+
+from salt.ext.six.moves import map
+
+# pylint: enable=import-error
+
+log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+__virtualname__ = 'allproc'
+
+
+def __virtual__():
+    if not HAS_PSUTIL:
+        return (False, 'cannot load ps beacon: psutil not available')
+    return __virtualname__
+
+
+def validate(config):
+    return True, 'No configuration yet required'
+
+
+def beacon(config):
+    '''
+    Scan for processes and fire events with all process data
+
+    Example Config
+
+    .. code-block:: yaml
+
+        beacons:
+          allproc: []
+    '''
+    ret = []
+    procs = set()
+    for proc in psutil.process_iter():
+        _name = proc.name()
+        procs.add(_name)
+    for name in procs:
+        ret.append({'name': name})
+    return ret
+
