05daadb1afd5780781f8fb5fb297c3742634aeeb
Thomas S Hatch <thatch45@gmail.com>
2019-05-22 15:11:08 -0600

    ensure hub.P is created even if persist is not enabled

--- umbra/mods/umbra/init.py
+++ umbra/mods/umbra/init.py
@@ -14,6 +14,7 @@ import asyncio
 def new(hub):
     hub.tools.conf.integrate(['umbra'], loader='yaml', cli='umbra', roots=True)
     hub.UP = {}
+    hub.P = {}
     hub.umbra.init.load_subs()
     hub.flows.init.load()
     hub.umbra.init.start()
